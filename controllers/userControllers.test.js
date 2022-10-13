ed252862d277d8562d17724b227d7349f8b462be
Jeel <45571080+j33l@users.noreply.github.com>
2022-10-12 18:59:21 -0700

    Sugnup user test case works

--- controllers/userControllers.test.js
+++ controllers/userControllers.test.js
@@ -1,25 +1,13 @@
 const request = require("supertest");
-const express = require("express");
+const app = require("../app");
 
-const app = express();
-
-app.post("/api/user", function (req, res) {
-  res.status(201).json({
-    _id: "6344ab15a4d9d6594ff6fe06",
-    name: "test user",
-    email: "test@test.com",
-    isAdmin: false,
-    pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-    token:
-      "eyJhbGciOiJIUzI1NiIsdfdfdfdfdkpXVCJ9.eyJpZCI6IjYzNDRhYjE1YTRkOWQ2NTk0ZmY2ZmUwNiIsImlhdCI6MTY2NTQ0NDYyOSwiZXhwIjoxNjY4MDM2NjI5fQ.KmbAIEQOf8RoZkDBRrn7I_el42Gw3VxGBw8Do55zuLg",
-  });
+test("Should signup a new user!", async () => {
+  await request(app)
+    .post("/api/user")
+    .send({
+      name: "testCseUser",
+      email: "testcaseuser@ex.com",
+      password: "testcaseuser",
+    })
+    .expect(201);
 });
-
-request(app)
-  .get("/user")
-  .expect("Content-Type", /json/)
-  .expect("Content-Length", "15")
-  .expect(200)
-  .end(function (err, res) {
-    if (err) throw err;
-  });
