046ddf82918f4c74579c16fad537afe0f25c3044
parthcsun <parth.savaj.356@my.csun.edu>
2022-11-26 19:52:25 -0800

    feat: add test cases of chat controller

--- controllers/chatControllers.test.js
+++ controllers/chatControllers.test.js
@@ -21,7 +21,6 @@ const testUser3 = {
   password: "123456",
 };
 
-
 // test("Should signup a new user!", async () => {
 //   await User.deleteMany({}); // delete all data from User collection the database to avoid "already exists" error
 
@@ -33,7 +32,6 @@ test("Should create new chat", async () => {
   let res1 = await request(app).post("/api/user").send(testUser).expect(201);
   let res2 = await request(app).post("/api/user").send(testUser2).expect(201);
   let res3 = await request(app).post("/api/user").send(testUser3).expect(201);
-
 
   let user1 = res1.body;
   let user2 = res2.body;
@@ -64,7 +62,6 @@ test("Should fetch all chats for a user", async () => {
 });
 
 test("Should send message to a chat", async () => {
-
   const user1 = await request(app)
     .post("/api/user/login")
     .send({
@@ -78,13 +75,88 @@ test("Should send message to a chat", async () => {
   console.log("chat", chat);
 
   await request(app)
-    .post("/api/chat/" )
+    .post("/api/chat/")
     .set("Authorization", `Bearer ${user1.body.token}`)
-    .send({ message: "test message",userId: user1.body._id})
+    .send({ message: "test message", userId: user1.body._id })
+    .expect(200);
+});
+
+test("Should fetch all messages for a chat", async () => {
+  const user1 = await request(app)
+    .post("/api/user/login")
+    .send({
+      email: testUser.email,
+      password: testUser.password,
+    })
+    .expect(200);
+
+  const chat = await Chat.find({
+    users: { $elemMatch: { $eq: user1.body._id } },
+  });
+
+  await request(app)
+    .get("/api/chat/")
+    .set("Authorization", `Bearer ${user1.body.token}`)
+    .expect(200);
+});
+
+test("Should rename a group", async () => {
+  const user1 = await request(app)
+    .post("/api/user/login")
+    .send({
+      email: testUser.email,
+      password: testUser.password,
+    })
+    .expect(200);
+
+  const chat = await Chat.findOne({
+    users: { $elemMatch: { $eq: user1.body._id } },
+  });
+  console.log("chat", chat);
+  await request(app)
+    .put("/api/chat/rename")
+    .set("Authorization", `Bearer ${user1.body.token}`)
+    .send({ chatId: chat._id, chatName: "new name" })
     .expect(200);
 });
 
 
+test("Should add a user to a group", async () => {
+  const user1 = await request(app)
+    .post("/api/user/login")
+    .send({
+      email: testUser.email,
+      password: testUser.password,
+    })
+    .expect(200);
 
+  const chat = await Chat.findOne({
+    users: { $elemMatch: { $eq: user1.body._id } },
+  });
+  console.log("chat", chat);
+  await request(app)
+    .put("/api/chat/groupadd")
+    .set("Authorization", `Bearer ${user1.body.token}`)
+    .send({ chatId: chat._id, userId: user1.body._id })
+    .expect(200);
+});
 
+test("Should remove a user from a group", async () => {
+  const user1 = await request(app)
+    .post("/api/user/login")
+    .send({
+      email: testUser.email,
+      password: testUser.password,
+    })
+    .expect(200);
 
+  const chat = await Chat.findOne({
+    users: { $elemMatch: { $eq: user1.body._id } },
+  });
+  console.log("chat", chat);
+  await request(app)
+    .put("/api/chat/groupremove")
+    .set("Authorization", `Bearer ${user1.body.token}`)
+    .send({ chatId: chat._id, userId: user1.body._id })
+    .expect(200);
+});
