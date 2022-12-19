3ecc4b9d013141bc88dea22e76f467e1e970d409
Jeel <45571080+j33l@users.noreply.github.com>
2022-12-19 02:30:12 -0800

    completed all almost 100% code coverage with help of @parthcsun

--- config/db.js
+++ config/db.js
@@ -1,14 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const mongoDB = process.env.MONGO_URI;
 
 //Set up default mongoose connection
 exports.connectDB = async () => {
-    try {
-        await mongoose.connect(mongoDB);
-        console.info("Connected to mongodb");
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-
+  await mongoose.connect(mongoDB);
+  console.info("Connected to mongodb");
+};
